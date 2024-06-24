59a625e732196eec0829de4633fc4b1c37fed0d1
habr <habr@itu.dk>
2024-06-24 21:09:10 +0200

    conways game of life

--- src/routes/cellgame/+page.svelte
+++ src/routes/cellgame/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-  let dimensions = [10, 10]
+  let dimensions = [20, 20]
+  let playing = false
+
+  async function sleep(ms: number): Promise<void> {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 
   function generateEmptyGrid(x: number, y: number) {
     let out:number[][] = []
@@ -14,19 +19,67 @@
 
   let grid:number[][] = generateEmptyGrid(dimensions[0], dimensions[1])
 
+  let neighborRules = [
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0]
+  ]
+
+  function getCell(x: number, y: number) {
+    if (x >= 0 && y >= 0 && x < dimensions[0] && y < dimensions[0]) {
+      return grid[x][y] 
+    } else return 0
+  }
+
+  function countNeighbors(x: number, y: number) {
+    return (
+      getCell(x+1, y) + 
+      getCell(x+1, y+1) + 
+      getCell(x+1, y-1) + 
+      getCell(x, y+1) + 
+      getCell(x, y-1) + 
+      getCell(x-1, y) + 
+      getCell(x-1, y+1) + 
+      getCell(x-1, y-1)
+    )
+  }
+
+  function updateGrid() {
+    let newGrid:number[][] = generateEmptyGrid(dimensions[0], dimensions[1])
+    for (let i = 0; i < newGrid.length; i++) {
+      for (let j = 0; j < newGrid[i].length; j++) {
+        newGrid[i][j] = neighborRules[grid[i][j]][countNeighbors(i, j)]
+      }
+    }
+    console.log(newGrid)
+    grid = newGrid
+  }
+
   function swap(x: number, y: number) {
     grid[x][y] = grid[x][y] == 0 ? 1 : 0
     grid = grid
   }
+
+  function toggle() {
+    playing = !playing
+    play()
+  }
+
+  async function play() {
+    if (!playing) return
+    
+    await sleep(100)
+    updateGrid()
+    play()
+  }
 </script>
 
 <svelte:head>
-	<title>About</title>
+	<title>Cellgame</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>Cellular Automata Game!</h1>
+	<h1>Conways game of life!</h1>
 
   <div class="grid">
     {#each grid as column, i}
@@ -39,6 +92,18 @@
         {/each}
       </div>
     {/each}
+  </div>
+
+  <div class="button-row">
+    <button on:click={() => (grid = generateEmptyGrid(dimensions[0], dimensions[1]))}>
+      clear
+    </button>
+    <button on:click={updateGrid}>
+      step
+    </button>
+    <button on:click={toggle}>
+      {playing ? "pause" : "play"}
+    </button>
   </div>
 </div>
 
@@ -60,6 +125,11 @@
     aspect-ratio: 1 / 1;
     border-style: none;
     outline: 1px solid black;
+  }
+
+  .button-row {
+    display: flex;
+    justify-content: center;
   }
 
   .cell-0 {
